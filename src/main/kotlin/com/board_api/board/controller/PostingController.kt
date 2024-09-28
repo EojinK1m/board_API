13994683b9386a1de794852b6b8593c67d7db6ca
EojinK1m <eojin.dev@gmail.com>
2024-09-29 00:54:33 +0900

    UPDATE Post posting API

--- src/main/kotlin/com/board_api/board/controller/PostingController.kt
+++ src/main/kotlin/com/board_api/board/controller/PostingController.kt
@@ -1,7 +1,10 @@
 package com.board_api.board.controller
 
+import com.board_api.board.dto.PostingItemDto
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -11,5 +14,14 @@ class PostingController {
     @GetMapping
     fun getPostings(): String{
         return "hello world"
+    }
+
+    @PostMapping
+    fun createPosting(
+        @RequestBody postingReq: PostingItemDto
+    ): ResponseEntity<PostingItemDto> {
+        return ResponseEntity
+            .status(201)
+            .body(postingReq)
     }
 }
